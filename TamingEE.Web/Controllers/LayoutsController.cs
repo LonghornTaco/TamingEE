a52c9373c7824b365eb70a4a6516a3bd7a544cc8
rba\jwilkerson <jason.wilkerson@rbaconsulting.com>
2016-01-21 00:23:49 -0600

    Three layout components and one content component;

---
+++ TamingEE.Web/Controllers/LayoutsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Mvc.Controllers;
+using Sitecore.Mvc.Presentation;
+
+namespace TamingEE.Web.Controllers
+{
+   public class LayoutsController : SitecoreController
+   {
+      public ViewResult OneColumnSection()
+      {
+         return View();
+      }
+
+      public ViewResult TwoColumnSection()
+      {
+         return View();
+      }
+
+      public ViewResult ThreeColumnSection()
+      {
+         return View();
+      }
+
+      public ViewResult TextItem()
+      {
+         Item item = null;
+         if (!String.IsNullOrEmpty(RenderingContext.Current.Rendering.DataSource))
+         {
+            item = Sitecore.Context.Database.GetItem(new ID(RenderingContext.Current.Rendering.DataSource));
+         }
+         return View(item);
+      }
+   }
+}
